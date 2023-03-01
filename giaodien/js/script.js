e0075cd2cf47ebf378c1a1e9afcc17680cd1aea5
Hieu <www.duyhieu2012>
2023-03-01 09:57:02 +0700

    Bổ sung product & CSS & script

--- giaodien/js/script.js
+++ giaodien/js/script.js
@@ -13,3 +13,33 @@ itemsliderbar.forEach(function(menu,index){
         menu.classList.toggle("block")
     })
 })
+const bigImg = document.querySelector(".product-content-left-big-img img")
+const smallImg = document.querySelectorAll(".product-content-left-small-img img")
+smallImg.forEach(function(imgItem,X){
+    imgItemqw.addEventListener("click",function(){
+        bigImg.src = imgItem.src
+    })
+})
+
+
+
+const baoquan = document.querySelector(".baoquan")
+const chitiet = document.querySelector(".chitiet")
+if(baoquan){
+    baoquan.addEventListener("click",function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "block"
+    })
+}
+if(chitiet){
+    chitiet.addEventListener("click",function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "block"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "none"
+    })
+}
+const button = document.querySelector(".product-content-right-bottom-top")
+if(button){
+    button.addEventListener("click",function(){
+        document.querySelector(".product-content-right-bottom-content-big").classList.toggle("activeB")
+    })
+}
