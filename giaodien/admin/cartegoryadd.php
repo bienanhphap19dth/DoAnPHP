cf020e22240ad5854268143d41229bf4e3e2a1ad
khanhhien <mhkhanhhien0206@gmail.com>
2023-03-03 22:10:21 +0700

    bắt đầu code backend + thêm danh mục

---
+++ giaodien/admin/cartegoryadd.php
@@ -0,0 +1,18 @@
+<?php
+include "header.php";
+include "slider.php"
+?>
+
+<div class="admin-content-right">
+            <div class="admin-content-right-cartegory_add">
+                <h1>Thêm danh mục</h1>
+                <form action="" method="POST">
+                    <input type="cartegory_name" placeholder="Nhập tên danh mục">
+                        <!-- THÊM -->
+                    <button type ="submit">Thêm</button> 
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ giaodien/admin/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/863656e15a.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../admin/style.css">
+    <title>Danh mục sản phẩm </title>
+</head>
+<body>
+    <header>
+        <h1>TOP</h1>
+    </header>
+</body>
+</html>
