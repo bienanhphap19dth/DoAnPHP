3ce7b44b36a280e5cd69971dbad88299dc5f6dc5
Hieu <www.duyhieu2012>
2023-03-16 02:21:15 +0700

    Thao tác thêm sản phẩm (productadd, product_class) và database

--- giaodien/admin/productadd.php
+++ giaodien/admin/productadd.php
@@ -1,31 +1,61 @@
 <?php
 include "header.php";
 include "slider.php";
+include "class/product_class.php";
 ?>
+<?php
+$product = new product;
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+   // var_dump($_POST,$_FILES);
+//    echo '<pre>';
+//    echo print_r($_FILES);
+//    echo '</pre>';
+    $insert_product = $product ->insert_product($_POST,$_FILES);
+}
+?>
+
+
 <div class="admin-content-right">
             <div class="admin-content-right-product_add">
                 <h1>Thêm Sản Phẩm</h1>
                 <form action="" method="POST" enctype="multipart/form-data">
                     <label for="">Nhập Tên Sản Phẩm <span style="color:red;">*</span></label>
-                    <input type="text"> 
+                    <input name="product_name"required type="text"> 
                     <label for="">Chọn Danh Mục <span style="color:red;">*</span></label>
-                    <select name="" id="">
+                    <select name="cartegory_id" id="">
                         <option value="#">--Chọn--</option>
-                    </select>   
-                    <select name="" id="">
+                        <?php
+                         $show_cartegory = $product ->show_cartegory();
+                         if($show_cartegory){while($result = $show_cartegory -> fetch_assoc()){ 
+                         ?>
+                         <option value="<?php echo $result['cartegory_id'] ?>"><?php echo $result['cartegory_name'] ?></option>   
+                         <?php
+                         }}  
+                        ?>
+                    </select>
+                    <label for="">Chọn Loại Sản Phẩm <span style="color:red;">*</span></label>   
+                    <select name="brand_id" id="">
                         <label for="">Chọn Loại Sản Phẩm <span style="color:red;">*</span></label>
                         <option value="#">--Chọn--</option>
+                        <?php
+                         $show_brand = $product -> show_brand();
+                         if($show_brand) {while($result = $show_brand -> fetch_assoc()){ 
+                         ?>
+                         <option value="<?php echo $result['brand_id'] ?>"><?php echo $result['brand_name'] ?></option>   
+                         <?php
+                         }}  
+                        ?>
                     </select> 
                     <label for="">Giá Sản Phẩm <span style="color:red;">*</span></label>
-                    <input type="text"> 
+                    <input name="product_price"required type="text"> 
                     <label for="">Giá Khuyến Mãi<span style="color:red;">*</span></label>
-                    <input type="text">
+                    <input name="product_price_new"required type="text">
                     <label for="">Mô Tả Sản Phẩm <span style="color:red;">*</span></label>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                    <textarea required name="product_desc" id="" cols="30" rows="10"></textarea>
                     <label for="">Ảnh Sản Phẩm <span style="color:red;">*</span></label>
-                    <input type="file">
+                    <input name="product_img"required type="file">
                     <label for="">Ảnh Mô Tả <span style="color:red;">*</span></label>
-                    <input multiple type="file">
+                    <input name="product_img_desc"required multiple type="file">
                         <!-- THÊM -->
                     <button type ="submit">Thêm</button> 
                 </form>
--- giaodien/database/websitephp.sql
+++ giaodien/database/websitephp.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 12, 2023 lúc 06:10 PM
--- Phiên bản máy phục vụ: 10.4.27-MariaDB
--- Phiên bản PHP: 8.2.0
+-- Host: 127.0.0.1
+-- Generation Time: Mar 15, 2023 at 08:20 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `websitephp`
+-- Database: `websitephp`
 --
 CREATE DATABASE IF NOT EXISTS `websitephp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `websitephp`;
@@ -26,7 +26,7 @@ USE `websitephp`;
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tbl_brand`
+-- Table structure for table `tbl_brand`
 --
 
 CREATE TABLE `tbl_brand` (
@@ -35,10 +35,17 @@ CREATE TABLE `tbl_brand` (
   `brand_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `tbl_brand`
+--
+
+INSERT INTO `tbl_brand` (`brand_id`, `cartegory_id`, `brand_name`) VALUES
+(0, 10, 'Beo');
+
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tbl_cartegory`
+-- Table structure for table `tbl_cartegory`
 --
 
 CREATE TABLE `tbl_cartegory` (
@@ -47,7 +54,7 @@ CREATE TABLE `tbl_cartegory` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `tbl_cartegory`
+-- Dumping data for table `tbl_cartegory`
 --
 
 INSERT INTO `tbl_cartegory` (`cartegory_id`, `cartegory_name`) VALUES
@@ -59,31 +66,81 @@ INSERT INTO `tbl_cartegory` (`cartegory_id`, `cartegory_name`) VALUES
 (9, 'TIN TỨC'),
 (10, 'THÔNG TIN ');
 
+-- --------------------------------------------------------
+
 --
--- Chỉ mục cho các bảng đã đổ
+-- Table structure for table `tbl_product`
+--
+
+CREATE TABLE `tbl_product` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `cartegory_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `product_price` varchar(255) NOT NULL,
+  `product_price_new` varchar(255) NOT NULL,
+  `product_desc` varchar(5000) NOT NULL,
+  `product_img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+INSERT INTO `tbl_product` (`product_id`, `product_name`, `cartegory_id`, `brand_id`, `product_price`, `product_price_new`, `product_desc`, `product_img`) VALUES
+(5, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(6, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(7, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(8, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(9, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(10, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(11, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(12, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(13, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(14, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(15, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(16, 'a', 0, 0, '111', '33', 'aaa', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(17, 'aaa', 8, 0, '1111', '222', 'asdadas', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(18, 'aaa', 8, 0, '1111', '222', 'asdadas', 'z4146560328534_3319923a69bec226f52eb355e177b812.jpg'),
+(19, 'ccc', 7, 0, '1212', '12', 'aaaaaaa', 'sp8.jpg');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `tbl_brand`
+-- Indexes for table `tbl_brand`
 --
 ALTER TABLE `tbl_brand`
   ADD PRIMARY KEY (`brand_id`);
 
 --
--- Chỉ mục cho bảng `tbl_cartegory`
+-- Indexes for table `tbl_cartegory`
 --
 ALTER TABLE `tbl_cartegory`
   ADD PRIMARY KEY (`cartegory_id`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `tbl_cartegory`
+-- AUTO_INCREMENT for table `tbl_cartegory`
 --
 ALTER TABLE `tbl_cartegory`
-  MODIFY `cartegory_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `cartegory_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
